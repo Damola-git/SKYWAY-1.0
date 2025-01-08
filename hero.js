c77ff1532149ce7464f9ed1658e9268de6bd067a
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2025-01-08 20:40:14 +0100

    Update hero.js

--- hero.js
+++ hero.js
@@ -1,9 +1,1 @@
-let day= "Tuesday";
-switch(day){
-    case "monday":
-        console.log("Its monday");
-        break
-    case "Tuesday":
-        console.log("Its tuesday");
-        break
-}
+
